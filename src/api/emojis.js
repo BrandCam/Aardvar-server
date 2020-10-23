11491b945be6c55f4602ede8115e927e466c926f
BrandCam <ufc.campbell@gmail.com>
2020-10-22 19:44:58 -0500

    test endpoints

--- src/api/emojis.js
+++ src/api/emojis.js
@@ -1,9 +1,21 @@
 const express = require("express");
-
+const User = require("../models/User");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json(["😀", "😳", "🙄"]);
+router.get("/", async (req, res) => {
+  try {
+    console.log(req);
+    let user = await User.find({ email: req.body.email });
+
+    res.json({
+      ...user,
+    });
+  } catch (err) {
+    res.json({
+      message: err.message,
+      stack: err.stack,
+    });
+  }
 });
 
 module.exports = router;
