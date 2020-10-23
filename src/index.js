93df08034265a84deb205f9550388628471cd0ad
BrandCam <ufc.campbell@gmail.com>
2020-10-23 07:04:28 -0500

    heroku cant do ssl on the server....great

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const app = require("./app");
-const { createServer } = require("https");
-const fs = require("fs");
+const { createServer } = require("http");
+// const fs = require("fs");
 const { execute, subscribe } = require("graphql");
 const { SubscriptionServer } = require("subscriptions-transport-ws");
 const { makeExecutableSchema } = require("graphql-tools");
 const typeDefs = require("./GraphQL/typeDefs");
 const resolvers = require("./GraphQL/resolvers");
 
-const options = {
-  key: fs.readFileSync("key.pem"),
-  cert: fs.readFileSync("cert.pem"),
-};
+// const options = {
+//   key: fs.readFileSync("key.pem"),
+//   cert: fs.readFileSync("cert.pem"),
+// };
 
-const server = createServer(options, app);
+// const server = createServer(options, app);
+const server = createServer(app);
 const Schema = makeExecutableSchema({
   typeDefs,
   resolvers,
